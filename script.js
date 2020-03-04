e96aa3ccda51b4bcb9a1ed4fe74011a37de4c0fd
Hatori Shuntaro <daily33316@gmail.com>
2020-03-04 18:32:12 +0900

    ローディング画面の試作

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ if(e&&1===a.nodeType)while(c=e[d++])a.removeAttribute(c)}}),hb={set:function(a,b
 		$('.navbar').slideToggle()
 	})
 })(jQuery);
+
+// gifを表示し終わったらコンテンツを表示する
+window.setTimeout(LoadingComplete, 6000);
+
+function LoadingComplete() {
+	var loading = document.getElementById('loading');
+	var allcontent = document.getElementById('all-content');
+
+	allcontent.style.display = 'block';
+	loading.style.display = 'none';
+}
