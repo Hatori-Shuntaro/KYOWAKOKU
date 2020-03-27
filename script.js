1985e28af20b48fe46d55fbd39ecb3b1a93f12c3
Hatori Shuntaro <daily33316@gmail.com>
2020-03-27 15:36:36 +0900

    ローディングのGifを削除

--- script.js
+++ script.js
@@ -5,14 +5,3 @@
 		$('.navbar').slideToggle()
 	})
 })(jQuery);
-
-// gifを表示し終わったらコンテンツを表示する
-window.setTimeout(LoadingComplete, 6000);
-
-function LoadingComplete() {
-	var loading = document.getElementById('loading');
-	var allcontent = document.getElementById('all-content');
-
-	allcontent.style.display = 'block';
-	loading.style.display = 'none';
-}
